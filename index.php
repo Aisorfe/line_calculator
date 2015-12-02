84f2bf445de24d97a661cd1ca841f5e5cf772c92
Nikita Belousov <seriouscat1001@gmail.com>
2015-12-02 20:14:10 +0300

    исправил ошибки, добавил бутстрап

--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Лабораторная работа 2. Калькулятор.</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Строковый калькулятор.</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 
-    <form method="post">
-        <label for="calc">Введите выражение:</label><br><br>
-        <input type="text" name="expression"></input><br><br>
-        <input type="submit" value="Посчитать"><br><br>
-    </form>
+    <div class="container">
+        <br><br><br><br>
+        <div class="col-md-6 col-md-offset-3">
+            <form method="post">
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control" placeholder="Введите выражение..." name="expression">
+                    <span class="input-group-btn">
+                        <button class="btn btn-success" type="submit">Вычислить!</button>
+                    </span>
+                </div>
+            </form>
+        </div>
+    </div>
 
 <?php
 
@@ -19,13 +40,23 @@ $acceptable_chars = array(" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 function calculate($expression) {
     if ( $expression != '' ) {
         if ( !is_correct_expression($expression) ) {
-            echo 'Выражение содержит недопустимые символы.';
+            echo '<div class="col-md-6 col-md-offset-3">
+                    <br><br>
+                    <div class="alert alert-danger" role="alert">
+                        Выражение содержит недопустимые символы. 
+                    </div>
+                <div>';
             return;
         }
     }
     $outstring = get_polish_notation($expression);
     $result = calculate_polish_notation($outstring);
-    echo $result;
+    if ( $result ) {
+    echo '<div class="col-md-6 col-md-offset-3">
+            <br><br>
+            <div class="alert alert-success" role="alert">Результат: '.$result.'</div>
+        <div>';
+    }
 }
 
 function is_correct_expression($expression) {
@@ -106,7 +137,7 @@ function calculate_polish_notation($notation) {
             if ($notation[$i] == '-') $result = $a - $b;
             elseif ($notation[$i] == '+') $result = $a + $b;
             elseif ($notation[$i] == '*') $result = $a * $b;
-            elseif ($notation[$i] == '/') $result = $a / $b;
+            elseif ($notation[$i] == '/') $result = $b / $a;
             $calc_stack->push($result);
         }
     }
